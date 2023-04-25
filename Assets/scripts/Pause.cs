1eee944a80e7844613c6c87c4db0a59ca617149b
Ben Guile <bennyguile@gmail.com>
2023-04-25 15:34:14 +0100

    pause update
    added art, changed event system

--- Assets/scripts/Pause.cs
+++ Assets/scripts/Pause.cs
@@ -25,7 +25,7 @@ public class Pause : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Q))
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
             if (!isPaused)
             {

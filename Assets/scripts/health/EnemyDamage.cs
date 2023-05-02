f2fff7fbaa79c9d7f5e1a0261483b07a00c8c07d
Ben Guile <bennyguile@gmail.com>
2023-05-02 22:40:11 +0100

    enemies actually work

--- Assets/scripts/health/EnemyDamage.cs
+++ Assets/scripts/health/EnemyDamage.cs
@@ -10,6 +10,8 @@ public class EnemyDamage : MonoBehaviour
     public GameObject player;
     public float damage;
     public Combat C;
+    public float distanceBasic;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,37 +23,38 @@ public class EnemyDamage : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float distance = Vector2.Distance(this.transform.position, player.transform.position);
+        float distanceBasic = Vector2.Distance(this.transform.position, player.transform.position);
 
-        Debug.Log(distance);
-        if (distance < 6 && Input.GetKey(KeyCode.P))
+        //Debug.Log(distance);
+        if (distanceBasic < 4.5 && Input.GetKey(KeyCode.I))
         {
             //Destroy(gameObject);
-            Destroy(gameObject, 1.0f);
+            gameObject.SetActive(false);
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    /*private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Player") && (Input.GetKey(KeyCode.I)))
+        if (collision.gameObject.CompareTag("Player") && (Input.GetKey(KeyCode.P)))
         {
             //Debug.Log(C.ReturnIsAttacking());
-            gameObject.GetComponent<EnemyHealth>().health -= damage;
+            //gameObject.GetComponent<EnemyHealth>().health -= damage;
+            gameObject.SetActive(false);
 
-            if (Health.health < 0)
+            //if (Health.health < 0)
             {
-                Animator anim= gameObject.GetComponent<Animator>();
-                anim.SetTrigger("death");
-                Invoke("death", 1.5f);
+                //Animator anim= gameObject.GetComponent<Animator>();
+                //anim.SetTrigger("death");
+                //Invoke("death", 1.5f);
 
-                { /*Destroy(gameObject);*/ }
+                { Destroy(gameObject); }
             }
         }
 
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player") && (Input.GetKey(KeyCode.P)))
+        if (collision.gameObject.CompareTag("Player") && (Input.GetKey(KeyCode.I)))
         {
 
             gameObject.SetActive(false);
@@ -65,7 +68,7 @@ public class EnemyDamage : MonoBehaviour
             //{ Destroy(gameObject); }
 
         }
-    }
+    }*/
 
     private void death()
     {
